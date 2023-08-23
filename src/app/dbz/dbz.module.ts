ee9512ab079dc806ffd07589a8b89a9d4d0c4e9c
Jorge Guepud <jlguepudb@unal.edu.co>
2023-08-22 21:23:11 -0500

    Added interface character and add-character component

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    ListComponent
+    ListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
